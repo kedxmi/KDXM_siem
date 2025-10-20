bd5e1b94c450521874a2ab6ea811b4d99925a049
kedxmi <kedxmi@localhost.localdomain>
2025-10-20 14:58:18 +0300

    siem.py начало

--- siem.py
+++ siem.py
@@ -1,1 +1,19 @@
 
+import sqlite3
+
+# Создание или подключение к базе данных
+conn = sqlite3.connect('siem.db')
+cursor = conn.cursor()
+
+# Таблица для событий
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS events (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    type TEXT,
+    description TEXT,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+)
+''')
+
+conn.commit()
+print("[+] Database initialized.")
